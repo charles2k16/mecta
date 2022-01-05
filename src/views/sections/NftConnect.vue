3d81ffee2d63f4ab3ae5c989a8bcd89b491b2a9b
Charles2k16 <charlesprogram@gmail.com>
2022-01-05 08:12:58 +0000

    build trailer fix

--- src/views/sections/NftConnect.vue
+++ src/views/sections/NftConnect.vue
@@ -42,58 +42,56 @@ export default {
   background-size: cover;
   height: 800px;
 }
+.conn_btn {
+  background: rgb(234, 0, 217);
+  background: linear-gradient(
+    90deg,
+    rgba(234, 0, 217, 1) 0%,
+    rgba(10, 189, 198, 1) 100%
+  );
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  clip-path: polygon(
+    20% 0%,
+    80% 0%,
+    100% 0,
+    94% 53%,
+    89% 100%,
+    11% 100%,
+    3% 29%,
+    0 0
+  );
+
+  span {
+    color: whitesmoke;
+  }
+}
+.legendary_nft {
+  background: rgb(12, 12, 12);
+  color: whitesmoke;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 215px;
+  margin: 20px auto;
+  clip-path: polygon(
+    20% 0%,
+    80% 0%,
+    94% 1%,
+    100% 23%,
+    100% 100%,
+    0 100%,
+    0 35%,
+    0 0
+  );
+}
 .connect_div {
-  background: #ffffff36;
+  // background: #ffffff36;
   width: 280px;
   height: 350px;
   font-family: TitilliumBold;
-
-  .conn_btn {
-    background: rgb(234, 0, 217);
-    background: linear-gradient(
-      90deg,
-      rgba(234, 0, 217, 1) 0%,
-      rgba(10, 189, 198, 1) 100%
-    );
-    height: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    clip-path: polygon(
-      20% 0%,
-      80% 0%,
-      100% 0,
-      94% 53%,
-      89% 100%,
-      11% 100%,
-      3% 29%,
-      0 0
-    );
-
-    span {
-      color: whitesmoke;
-    }
-  }
-
-  .legendary_nft {
-    background: rgb(12, 12, 12);
-    color: whitesmoke;
-    height: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 215px;
-    margin: 20px auto;
-    clip-path: polygon(
-      20% 0%,
-      80% 0%,
-      94% 1%,
-      100% 23%,
-      100% 100%,
-      0 100%,
-      0 35%,
-      0 0
-    );
-  }
 }
 </style>
